7e7a78e24f03aba5bf60b7665004df7a7c902748
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2023-03-13 13:46:33 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import sklearn
+import pickle
+model = pickle.load(open(model.pickle,"rb"))
+st.balloons()
+st.snow()
+st.title('Revenue Prediction')
+x = st.number_input('Input Temperature' )
+if st.button("Predict"):
+   y = model.predict(x.reshape(-1,1))
+   st.success(y)
