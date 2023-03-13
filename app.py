143c342c61929ffca83ffd1e6ea7d34b186c7a32
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2023-03-13 13:49:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import sklearn
 import pickle
+import numpy as np 
 model = pickle.load(open("model.pickle","rb"))
 st.balloons()
 st.snow()
 st.title('Revenue Prediction')
 x = st.number_input('Input Temperature' )
+x = np.array(x)
 if st.button("Predict"):
    y = model.predict(x.reshape(-1,1))
    st.success(y)
